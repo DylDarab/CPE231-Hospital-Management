a7105086b9edb2237b643933dbd1df12dbd17936
PackHubTH <aussadawut2545@gmail.com>
2022-04-23 03:00:56 +0700

    login api

--- pages/api/login.js
+++ pages/api/login.js
@@ -1,9 +1,9 @@
 import db from '../../db'
 export default async function handler (req, res)
 {
-   if(req.method=="GET")
+   if(req.method=="POST")
    {
-       let result = await db.query('SELECT * FROM "public"."Device"')
+       let result = await db.query('SELECT * FROM "public"."Staff"')
        res.json(result.rows)
    }
 }
