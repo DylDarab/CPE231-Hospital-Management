9dfe455e77db16285851ae38164dc6b2ce0a6213
DylDarab <apicha.oap@gmail.com>
2022-04-23 03:13:17 +0700

    login endpoint

--- pages/api/login.js
+++ pages/api/login.js
@@ -1,9 +1,25 @@
+import { responseSymbol } from 'next/dist/server/web/spec-compliant/fetch-event'
 import db from '../../db'
 export default async function handler (req, res)
 {
    if(req.method=="POST")
    {
-       let result = await db.query('SELECT * FROM "public"."Staff"')
-       res.json(result.rows)
+        let result = await db.query('SELECT * FROM "public"."Staff" WHERE username = $1'
+                    ,[req.body.username])
+       if (result.rows.length!=1)
+        {
+            res.status(404).send("Username not found")
+        }
+        else
+        {
+           if (result.rows[0].password==req.body.password)
+            {
+                res.json(result.rows[0])
+            }
+            else
+            {
+                res.status(404).send("Wrong Password")
+            }
+        }
    }
 }
