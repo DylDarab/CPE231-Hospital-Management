eb17f167696e022e7f72e7cc4a73aae0cb6704f4
DylDarab <apicha.oap@gmail.com>
2022-04-25 22:22:47 +0700

    why I push

--- pages/schedule.js
+++ pages/schedule.js
@@ -1,14 +1,24 @@
-import { Image } from '@chakra-ui/react'
+import { Image, Container, Heading } from '@chakra-ui/react'
 import axios from 'axios'
 import Navbar from '../component/navbar'
 
 export default function Home(props)
 {
+
+    let container = {
+        width: '100%',
+        marginLeft: '360px',
+        marginTop: '50px',
+    }
+
     return (
         <div>
             <Navbar />
-
-            dashboard
+            <Container sx={container}>
+                <Heading>
+                    Schedule
+                </Heading>
+            </Container>
         </div>
     )
 }
