cd80bbf71ecae77b093b3b7ff880342e170af007
DylDarab <apicha.oap@gmail.com>
2022-04-26 06:28:13 +0700

    update url

--- url.js
+++ url.js
@@ -1,1 +1,2 @@
-export default 'http://localhost:3000'
+// export default 'http://localhost:3000'
+export default 'https://fallenangelhospitalmanagementbroforcpe34projectinsemes-dyldarab.vercel.app/'
