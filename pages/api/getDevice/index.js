da17403bc9bf3b0f7f3a9d6b07e1f95b34827144
DylDarab <apicha.oap@gmail.com>
2022-05-15 00:39:31 +0700

    getDevice and getMedicine

--- pages/api/getDevice/index.js
+++ pages/api/getDevice/index.js
@@ -7,9 +7,17 @@ export default async (req, res) =>
     let search = decode(req.headers.search||'').toLowerCase()
     if (req.method === 'GET')
     {
-        let result = await db.query(`SELECT "Device".*,CEILING(COUNT(*) OVER()/10) as page_amount FROM "public"."Device"
-                    WHERE (LOWER(CONCAT("device_name")) LIKE '%${search}%')
-                    ORDER BY "deviceID" ASC LIMIT 10 OFFSET $1 `, [(page - 1) * 10])
-         res.json(result.rows)
+        if (page == 0)
+        {
+            let result = await db.query(`SELECT * FROM "public"."Device" `)
+            res.json(result.rows)
+        }
+        else
+        {
+            let result = await db.query(`SELECT "Device".*,CEILING(COUNT(*) OVER()/10) as page_amount FROM "public"."Device"
+                        WHERE (LOWER(CONCAT("device_name")) LIKE '%${search}%')
+                        ORDER BY "deviceID" ASC LIMIT 10 OFFSET $1 `, [(page - 1) * 10])
+             res.json(result.rows)
+        }
     }
 }
