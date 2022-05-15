c35ce649f24819c5041574da28b8650f7edc3081
DylDarab <apicha.oap@gmail.com>
2022-05-15 21:25:58 +0700

    getPrescription

--- pages/api/getPrescription/index.js
+++ pages/api/getPrescription/index.js
@@ -3,23 +3,24 @@ import { decode } from 'js-base64'
 
 export default async (req, res) =>
 {
-    // ยังไม่ได้เช็ค อย่าพึ่งใช้นะจ๊ะ
-    let appointmentid = req.headers.appointmentid
-
     if (req.method === 'GET')
     {
-        const medicine = await db.query(`
-        SELECT "MedicineWithdraw".*  FROM "public"."MedicineWithdraw" LEFT JOIN
-        "public"."Appointment" ON "Appointment"."appointmentID" = "MedicineWithdraw"."appointmentID"
-        WHERE "MedicineWithdraw"."appointmentID" = $1`, [appointmentid])
+        let page = req.headers.page
+        let search = decode(req.headers.search || '').toLowerCase()
 
-        const prescription = await db.query(`
-        SELECT "DeviceWithdraw".*  FROM "public"."DeviceWithdraw" LEFT JOIN
-        "public"."Appointment" ON "Appointment"."appointmentID" = "DeviceWithdraw"."appointmentID"
-        WHERE "DeviceWithdraw"."appointmentID" = $1`, [appointmentid])
-
-        res.json({
-            medicine: medicine.rows, prescription: prescription.rows
-        })
+        let result = await db.query(`
+            SELECT "Appointment".*,"Patient"."firstname" AS patient_firstname,"Patient"."lastname" AS patient_lastname,
+                    "Patient"."profile_img" AS patient_profile_img,"Room"."roomName", "Staff".firstname AS staff_firstname,
+                    "Staff".lastname AS staff_lastname, "Staff"."profile_img" AS staff_profile_img,
+                    CEILING(COUNT(*) OVER()/10) as page_amount FROM "Appointment"
+                    LEFT JOIN "Patient" ON "Patient"."patientID" = "Appointment"."patientID"
+                    LEFT JOIN "Staff" ON "Staff"."staffID" = "Appointment"."staffID"
+                    LEFT JOIN "public"."RoomUse" ON "RoomUse"."appointmentID" = "Appointment"."appointmentID"
+                    LEFT JOIN "public"."Room" ON "Room"."roomID" = "RoomUse"."roomID"
+                    WHERE "Appointment"."summary" IS NOT NULL AND "Appointment"."isCompleted" = false AND
+                    (LOWER(CONCAT("Patient".firstname,' ',"Patient".lastname)) LIKE '%${search}%')
+                    ORDER BY "appointmentID" ASC LIMIT 10 OFFSET $1
+        `, [(page - 1) * 10])
+        res.json(result.rows)
     }
 }
