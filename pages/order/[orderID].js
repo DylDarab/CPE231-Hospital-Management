06d0fc7eb108bd1c342c409ed8c7511d57e6772b
PackHubTH <aussadawut2545@gmail.com>
2022-05-15 21:18:49 +0700

    fix error appID

--- pages/order/[orderID].js
+++ pages/order/[orderID].js
@@ -1,12 +1,19 @@
 import { useRouter } from 'next/router'
 import
     {
-        Box, Button, Flex, FormControl, FormErrorMessage, FormLabel, Input, InputGroup, InputLeftElement, IconButton,
-        InputRightElement, Heading, HStack, Radio, Select, SimpleGrid, Text, Textarea, Stack, RadioGroup, VStack,
-        NumberInput, NumberInputField, NumberInputStepper, NumberIncrementStepper, NumberDecrementStepper, 
+        Avatar, Box, Button, Checkbox, Flex, FormControl, FormErrorMessage, FormLabel, IconButton, Input, InputGroup, InputLeftElement, 
+    Menu,MenuButton,MenuList,MenuItem,MenuItemOption,MenuGroup,MenuOptionGroup,MenuDivider,
+    NumberInput,NumberInputField,NumberInputStepper,NumberIncrementStepper,NumberDecrementStepper,
+    InputRightElement, Heading, HStack, VStack, Radio, Select, SimpleGrid, Text, Textarea, Stack, RadioGroup, ButtonGroup,
+    useToast 
     } from '@chakra-ui/react'
-
-import { ChevronDownIcon, AddIcon } from '@chakra-ui/icons'
+import {
+        AutoComplete,
+        AutoCompleteInput,
+        AutoCompleteItem,
+        AutoCompleteList,
+      } from "@choc-ui/chakra-autocomplete";
+import { ChevronDownIcon, AddIcon, CloseIcon } from '@chakra-ui/icons'
 import axios from 'axios'
 import Colour from '../../Colour'
 import { useState } from 'react'
@@ -14,7 +21,7 @@ import url from '../../url'
 
 export default (props) =>
 {
-
+    const { organizationData, medicineData, deviceData } = props
     const router = useRouter()
     console.log(props)
 
@@ -76,82 +83,100 @@ export default (props) =>
 
     const [error, setError] = useState(false)
     const [isEdit, setIsEdit] = useState(false)
-    const [form, setForm] = useState({
-        organization: props.orderData.organization_name,
-        medicine: props.orderData.medicine_name,
-        pricemedicine: props.orderData.o_priceperunit,
-        amountmedicine: props.orderData.amount,
-        device: props.orderData.device_name,
-        pricedevice: props.orderData.o_priceperunit,
-        amountdevice: props.orderData.amount
-    })
+    const [isSubmit, setIsSubmit] = useState(false)
 
-    console.log(form)
+    const buttonStyle = (bgColor, textColor) => {
+        return {
+            bg: bgColor,
+            color: textColor,
+            _hover: {filter: 'brightness(0.9)'},
+            transition:'all 0.2s cubic-bezier(.08,.52,.52,1)',
+        }
+    }
+
     return (
         <div style={{ backgroundColor: Colour.AlmostWhite }}>
             <Box sx={container} >
                 <Heading>
-                    OrderID {props.orderData.orderID}
+                    Add order
                 </Heading>
                 <Box sx={line}></Box>
             </Box>
             <Flex sx={container2}>
                 <Flex sx={container3}>
-                    <Heading as='h4' size='md'>Medicine Information</Heading>
+                    <Heading as='h4' size='md'>Order ID: </Heading>
 
                     <Box>
-                        <VStack spacing={4}>
-                            <HStack spacing={4} w='100%'>
-                                <FormControl isRequired isInvalid={error && !form.organization} w='50%'>
+                        <VStack spacing={4} align='flex-start'>
+                            {/* <HStack spacing={4} w='100%'> */}
+                                <FormControl>
                                     <FormLabel htmlFor='organization'>Organization</FormLabel>
-                                    <Input id='organization' value={form.organization}
-                                    isDisabled={!isEdit} _disabled={{opacity: 0.8}}
-                                    onChange={(e)=>{setForm({...form, organization: e.target.value})}}
+                                    <Input id='organization' 
+                                    isDisabled={isSubmit} _disabled={{opacity: 0.8}}
+                                    // onChange={(e)=>{setForm({...form, organization: e.target.value})}}
                                      />
                                 </FormControl>
+                            {/* </HStack> */}
+
+                            <HStack>
+                                <Heading as='h4' size='sm'>Medicine</Heading>
+                                <Button sx={buttonStyle(Colour.Green, Colour.White)} size='xs' rightIcon={<AddIcon />}
+                                    isDisabled={isSubmit} _disabled={{opacity: 0.8}}
+                                    onClick={()=>{setMedicineList([...medicineList, 
+                                        {"medicineID": "", "name": "", "price": "", "amount": "", "type": "used", "note": ""}])}}
+                                >
+                                    Add
+                                </Button>
+                            </HStack>
+
+                            <HStack spacing={4} w='100%' justify='center'>
+                                <FormControl>
+                                        <FormLabel>Medicine</FormLabel>
+                                        <Input
+                                            isDisabled={isSubmit} _disabled={{opacity: 0.8}}
+                                            // onChange={(e)=>{setForm({...form, medicine: e.target.value})}}
+                                        />
+                                </FormControl>
+                                <FormControl w='50%'>
+                                    <FormLabel>Price</FormLabel>
+                                    <NumberInput min={1} 
+                                        isDisabled={isSubmit} 
+                                        // onChange={(e)=>{setForm({...form, amountmedicine: e.target.value})}}
+                                    >
+                                        <NumberInputField/>
+                                        <NumberInputStepper>
+                                        <NumberIncrementStepper />
+                                        <NumberDecrementStepper />
+                                        </NumberInputStepper>
+                                    </NumberInput>
+                                </FormControl>
+                                <FormControl w='50%'>
+                                    <FormLabel>Amount</FormLabel>
+                                    <NumberInput min={1} 
+                                        isDisabled={isSubmit} 
+                                        // onChange={(e)=>{setForm({...form, amountmedicine: e.target.value})}}
+                                    >
+                                        <NumberInputField/>
+                                        <NumberInputStepper>
+                                        <NumberIncrementStepper />
+                                        <NumberDecrementStepper />
+                                        </NumberInputStepper>
+                                    </NumberInput>
+                                </FormControl>
+                                <IconButton sx={buttonStyle(Colour.Red, Colour.White)} size='sm' icon={<CloseIcon />} />
                             </HStack>
 
                             <HStack spacing={4} w='100%'>
-                            <FormControl isInvalid={error && !form.medicine} w='50%'>
-                                    <FormLabel htmlFor='medicine'>Medicine</FormLabel>
-                                    <Input id='medicine' value={form.medicine}
-                                    isDisabled={!isEdit} _disabled={{opacity: 0.8}}
-                                    onChange={(e)=>{setForm({...form, medicine: e.target.value})}}
-                                     />
-                            </FormControl>
-                            <FormControl isInvalid={error && !form.pricemedicine}>
-                                    <FormLabel htmlFor='price-medicine'>Price</FormLabel>
-                                    <Input id='price-medicine' value={form.pricemedicine}
-                                    isDisabled={!isEdit} _disabled={{opacity: 0.8}}
-                                    onChange={(e)=>{setForm({...form, pricemedicine: e.target.value})}}
-                                     />
-                            </FormControl>
-                            <FormControl isInvalid={error && !form.amountmedicine}>
-                                <FormLabel htmlFor='amount-medicine'>Amount</FormLabel>
-                                <NumberInput max={1000000} min={1} 
-                                    id='amount-medicine' value={form.amountmedicine} isDisabled={!isEdit} 
-                                    onChange={(e)=>{setForm({...form, amountmedicine: e.target.value})}}>
-                                    <NumberInputField/>
-                                    <NumberInputStepper>
-                                    <NumberIncrementStepper />
-                                    <NumberDecrementStepper />
-                                    </NumberInputStepper>
-                                </NumberInput>
-                            </FormControl>
-                            <IconButton aria-label='Add' icon={<AddIcon />} />
-                            </HStack>
-
-                            <HStack spacing={4} w='100%'>
-                            <FormControl isInvalid={error && !form.device} w='50%'>
+                            <FormControl w='50%'>
                                     <FormLabel htmlFor='device'>Device</FormLabel>
-                                    <Input id='device' value={form.device}
+                                    <Input id='device' 
                                     isDisabled={!isEdit} _disabled={{opacity: 0.8}}
-                                    onChange={(e)=>{setForm({...form, device: e.target.value})}}
+                                    // onChange={(e)=>{setForm({...form, device: e.target.value})}}
                                      />
                             </FormControl>
-                            <FormControl isInvalid={error && !form.pricedevice}>
+                            <FormControl>
                                     <FormLabel htmlFor='price-device'>Price</FormLabel>
-                                    <Input id='price-device' value={form.pricedevice}
+                                    <Input id='price-device' 
                                     isDisabled={!isEdit} _disabled={{opacity: 0.8}}
                                     onChange={(e)=>{setForm({...form, pricedevice: e.target.value})}}
                                      />
@@ -159,7 +184,7 @@ export default (props) =>
                             <FormControl isInvalid={error && !form.amountdevice}>
                                 <FormLabel htmlFor='amount-device'>Amount</FormLabel>
                                 <NumberInput max={1000000} min={1} 
-                                    id='amount-device' value={form.amountdevice} isDisabled={!isEdit} 
+                                    id='amount-device'  isDisabled={!isEdit} 
                                     onChange={(e)=>{setForm({...form, amountdevice: e.target.value})}}>
                                     <NumberInputField />
                                     <NumberInputStepper>
@@ -178,12 +203,35 @@ export default (props) =>
     )
 }
 
-export const getServerSideProps = async (context)=>{
-    let orderID = context.params.orderID
-    const orderData = await axios.get(`${url}/api/getOrder/${orderID}`)
+export const getStaticPaths = async () => {
+    const res = await axios.get(`${url}/api/getOrder/path`);
+    const paths = res.data.map((item) => ({
+        params: {
+            orderID: item.orderID
+        }
+    }));
+    return { paths, fallback: false };
+}
+
+export const getStaticProps = async (context)=>{
+    const id = context.params.orderID;
+    const organization = await axios.get(`${url}/api/getOrganization`);
+    const medicine = await axios.get(`${url}/api/getMedicine`, {
+        headers: {
+            "page": 0,       
+        }})
+    const device = await axios.get(`${url}/api/getDevice`, {
+        headers: {
+            "page": 0,       
+        }})
     return {
         props: {
-            orderData: orderData.data,
+            organizationData: JSON.parse(JSON.stringify(organization.data)),
+            medicineData: JSON.parse(JSON.stringify(medicine.data)),
+            deviceData: JSON.parse(JSON.stringify(device.data))
+            // data: data.data[0],
+            // medicine: medicine.data,
+            // device: device.data
         }
     }
 }
