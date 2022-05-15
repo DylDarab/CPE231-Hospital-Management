c737610897c5f54a124fb657a7b565114c287528
DylDarab <apicha.oap@gmail.com>
2022-05-15 14:19:05 +0700

    getLog let's go

---
+++ pages/api/getLog.js
@@ -0,0 +1,26 @@
+import db from '../../db'
+
+export default async (req, res) =>
+{
+    if (req.method === 'GET')
+    {
+        let page = req.headers.page
+        if (page == 0)
+        {
+            let result = await db.query(`
+            SELECT "Log".*,"Staff"."firstname" AS staff_firstname,"Staff"."lastname" AS staff_lastname
+            FROM "public"."Log" LEFT JOIN "public"."Staff" ON "Staff"."staffID" = "Log"."staffID"
+            `)
+            res.json(result.rows)
+        }
+        else
+        {
+            let result = await db.query(`
+            SELECT "Log".*,"Staff"."firstname" AS staff_firstname,"Staff"."lastname" AS staff_lastname
+            FROM "public"."Log" LEFT JOIN "public"."Staff" ON "Staff"."staffID" = "Log"."staffID"
+            LIMIT 10 OFFSET ${page}
+            `)
+            res.json(result.rows)
+        }
+    }
+}
