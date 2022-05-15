ba8209e53b9046d1dbac2ab5f754200e3ad8cb72
DylDarab <apicha.oap@gmail.com>
2022-05-15 22:49:01 +0700

    appointmentComplete

---
+++ pages/api/appointmentComplete.js
@@ -0,0 +1,11 @@
+import db from '../../db'
+
+export default async (req,res)=>{
+    let appointmentID = req.headers.appointmentid
+    if(req.method === 'POST'){
+        let result = await db.query(`
+        UPDATE "public"."Appointment" SET "isCompleted" = true WHERE "appointmentID" = $1
+        `,[appointmentID])
+        res.json(result.rows)
+    }
+}
