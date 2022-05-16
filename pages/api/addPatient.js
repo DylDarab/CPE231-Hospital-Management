3242008540caf251b0537765933420e1bc5fe05d
DylDarab <apicha.oap@gmail.com>
2022-05-16 14:06:39 +0700

    bro

--- pages/api/addPatient.js
+++ pages/api/addPatient.js
@@ -10,7 +10,7 @@ export default async (req, res)=>
         if(firstname && lastname && dob && gender && address && phone && citizenID &&
             EC_name && EC_relationship && EC_phone && blood)
         {
-            let result = await db.query(`INSERT INTO "public"."Staff" ("firstname", "lastname", "birthDate", 
+            let result = await db.query(`INSERT INTO "public"."Patient" ("firstname", "lastname", "birthDate", 
                             "gender", "address", "phone_number", "citizenID", "EC_name", 
                             "EC_Relationship", "EC_phone", "allergy", "bloodGroup", "med_history", "insurance") 
                             VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14) RETURNING *`,
--- pages/api/addStaff.js
+++ pages/api/addStaff.js
@@ -7,10 +7,10 @@ export default async (req, res) =>
 
     if (req.method == "POST")
     {
-        if (firstname && lastname && dob && gender && address && phone && citizenID &&
-            EC_name && EC_relationship && EC_phone && blood)
+        if (firstname && lastname && dob && citizenID && license_number && phone_number && salary &&
+            username && password && email && profile_img && positionID && departmentID)
         {
-            let result = await db.query(`INSERT INTO "Patient" ("firstname", "lastname", "citizenID",
+            let result = await db.query(`INSERT INTO "Staff" ("firstname", "lastname", "citizenID",
             "birthDate","license_number","phone_number","salary","username","password","email",
             "profile_img","positionID","departmentID") VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13) RETURNING *`
                 , [firstname, lastname, citizenID, dob, license_number, phone_number, salary,
